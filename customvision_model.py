83df3de88450c73d59f4a8547e4b08faab8999c3
kzx <samsamsamrox1212@gmail.com>
2021-08-06 22:25:15 +0300

    removed prediction url key

--- customvision_model.py
+++ customvision_model.py
@@ -3,9 +3,9 @@ import json
 import numpy as np
 
 
-pred_url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/a8c3b641-3ec2-41c5-85d4-db922d488dbb/url?iterationId=c3e9edd5-0748-4a30-a58f-b2b102d41bec"
+pred_url = "REMOVED"
 headers = {
-            'Prediction-Key': 'c070a93c50664a85b82c114278b4e4c1',
+            'Prediction-Key': '##',
             'Content-Type': 'application/json'
             }
 
